5c760217ca3be9314bf03de4e0262f5d03cb8a51
kmvignesh <kmvignesh94@gmail.com>
2019-04-06 21:15:07 +0530

    Added Forgot Password

--- app/src/main/java/in/codeandroid/firebasedemo/LoginActivity.kt
+++ app/src/main/java/in/codeandroid/firebasedemo/LoginActivity.kt
@@ -1,10 +1,13 @@
 package `in`.codeandroid.firebasedemo
 
+import android.content.DialogInterface
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AlertDialog
 import android.util.Log
 import android.util.Patterns
+import android.widget.EditText
 import android.widget.Toast
 import com.google.firebase.FirebaseApp
 import com.google.firebase.auth.FirebaseAuth
@@ -29,6 +32,36 @@ class LoginActivity : AppCompatActivity() {
             doLogin()
         }
 
+        btn_forgot_password.setOnClickListener {
+            val builder = AlertDialog.Builder(this)
+            builder.setTitle("Forgot Password")
+            val view = layoutInflater.inflate(R.layout.dialog_forgot_password,null)
+            val username = view.findViewById<EditText>(R.id.et_username)
+            builder.setView(view)
+            builder.setPositiveButton("Reset", DialogInterface.OnClickListener { _, _ ->
+                forgotPassword(username)
+            })
+            builder.setNegativeButton("close", DialogInterface.OnClickListener { _, _ ->  })
+            builder.show()
+        }
+
+    }
+
+    private fun forgotPassword(username : EditText){
+        if (username.text.toString().isEmpty()) {
+            return
+        }
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(username.text.toString()).matches()) {
+            return
+        }
+
+        auth.sendPasswordResetEmail(username.text.toString())
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                   Toast.makeText(this,"Email sent.",Toast.LENGTH_SHORT).show()
+                }
+            }
 
     }
 
