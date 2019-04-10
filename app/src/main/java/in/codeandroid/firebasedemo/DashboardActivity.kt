d538114ad47882c7ab7363482f9a2f97877767c2
kmvignesh <kmvignesh94@gmail.com>
2019-04-10 22:56:05 +0530

    Test DB and FireStore

--- app/src/main/java/in/codeandroid/firebasedemo/DashboardActivity.kt
+++ app/src/main/java/in/codeandroid/firebasedemo/DashboardActivity.kt
@@ -7,17 +7,101 @@ import android.util.Log
 import android.widget.Toast
 import com.google.firebase.auth.EmailAuthProvider
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.*
 import kotlinx.android.synthetic.main.activity_dashboard.*
+import com.google.firebase.firestore.DocumentChange
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Query
+import com.google.firebase.firestore.QuerySnapshot
+import java.util.*
+
 
 class DashboardActivity : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
+    var TAG = "DashboardActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
 
         auth = FirebaseAuth.getInstance()
+
+        if (auth.uid != null) {
+
+
+            val database = FirebaseDatabase.getInstance()
+            val myRef = database.getReference(auth.uid!!)
+            val productRef = myRef.child("products")
+            val product = Product()
+            product.productName = "product"
+            product.price = 10.0
+            product.createdAt = Date().time
+            product.description = "description"
+            // For creating list under "products"
+            myRef.child("products").push().setValue(product)
+            // For storing values directly under uid
+//            myRef.setValue(product)
+
+            productRef.addValueEventListener(object : ValueEventListener {
+                override fun onDataChange(dataSnapshot: DataSnapshot) {
+                    // This method is called once with the initial value and again
+                    // whenever data at this location is updated.
+                    for (postSnapshot in dataSnapshot.children) {
+                        val value = postSnapshot.getValue(Product::class.java)
+                        Log.d(TAG, "Value is: $value")
+                    }
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+                    // Failed to read value
+                    Log.w(TAG, "Failed to read value.", error.toException())
+                }
+            })
+
+
+//            // Access a Cloud Firestore instance from your Activity
+            val db = FirebaseFirestore.getInstance()
+//            // Create a new user with a first and last name
+
+//// Add a new document with a generated ID
+//            db.collection(auth.uid!!)
+//                .addSnapshotListener { snapshots, e ->
+//                    if (e != null) {
+//                        Log.w(TAG, "listen:error", e)
+//                    }
+//
+//                    for (dc in snapshots!!.documentChanges) {
+//                        when (dc.type) {
+//                            DocumentChange.Type.ADDED -> Log.d(TAG, "New city: " + dc.document.data)
+//                            DocumentChange.Type.MODIFIED -> Log.d(TAG, "Modified city: " + dc.document.data)
+//                            DocumentChange.Type.REMOVED -> Log.d(TAG, "Removed city: " + dc.document.data)
+//                        }
+//                    }
+//                }
+
+//            db.collection(auth.uid!!)
+//                .add(product)
+//                .addOnSuccessListener { documentReference ->
+//                    Log.d(TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
+//                }
+//                .addOnFailureListener { e ->
+//                    Log.w(TAG, "Error adding document", e)
+//                }
+//            db.collection(auth.uid!!)
+//                .orderBy("createdAt", Query.Direction.DESCENDING)
+//                .get()
+//                .addOnSuccessListener { result ->
+//                    for (document in result) {
+//                        Log.d(TAG, "${document.id} => ${document.data}")
+//                    }
+//                }
+//                .addOnFailureListener { exception ->
+//                    Log.d(TAG, "Error getting documents: ", exception)
+//                }
+        }
+
+
         btn_change_password.setOnClickListener {
             changePassword()
         }
@@ -45,7 +129,8 @@ class DashboardActivity : AppCompatActivity() {
                                 user?.updatePassword(et_new_password.text.toString())
                                     ?.addOnCompleteListener { task ->
                                         if (task.isSuccessful) {
-                                            Toast.makeText(this, "Password changed successfully.", Toast.LENGTH_SHORT).show()
+                                            Toast.makeText(this, "Password changed successfully.", Toast.LENGTH_SHORT)
+                                                .show()
                                             auth.signOut()
                                             startActivity(Intent(this, LoginActivity::class.java))
                                             finish()
---
+++ app/src/main/java/in/codeandroid/firebasedemo/Product.kt
@@ -0,0 +1,10 @@
+package `in`.codeandroid.firebasedemo
+
+class Product() {
+
+    var createdAt: Long = 0
+    var productName = ""
+    var description = ""
+    var price = 0.0
+
+}
